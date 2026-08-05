c618c76d29777fba9d0f875becc61f297e46c114
DevDaio <t.adickes@protonmail.com>
2026-08-05 09:19:28 +0200

    Chore: Praesentation-Ordner in .gitignore, Duplikat-Import entfernt

--- Invoice_Management_BACKEND/src/test/java/com/example/Invoice_Management_SPRING_REACT_PSQL/InvoiceManagementSpringReactPsqlApplicationTests.java
+++ Invoice_Management_BACKEND/src/test/java/com/example/Invoice_Management_SPRING_REACT_PSQL/InvoiceManagementSpringReactPsqlApplicationTests.java
@@ -1,7 +1,6 @@
 package com.example.Invoice_Management_SPRING_REACT_PSQL;
 
 import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
